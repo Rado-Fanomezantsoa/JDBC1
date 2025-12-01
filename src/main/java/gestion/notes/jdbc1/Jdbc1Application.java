df6a57289d0b16af4dd5dcba0ff16356c34b49f8
Rado <hei.fanomezantsoa@gmail.com>
2025-12-01 11:45:32 +0300

    first commit

---
+++ src/main/java/gestion/notes/jdbc1/Jdbc1Application.java
@@ -0,0 +1,13 @@
+package gestion.notes.jdbc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdbc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdbc1Application.class, args);
+	}
+
+}
